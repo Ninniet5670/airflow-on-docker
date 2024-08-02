16feea590d7170db81a87578f674f62f5fa11b3d
PROENÇA, Marco <contato.marcoproenca@gmail.com>
2024-08-02 17:21:46 -0300

    more about jinja and cron

--- dags/my_first_airflow_docker.py
+++ dags/my_first_airflow_docker.py
@@ -1,17 +1,20 @@
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.macros import ds_add
 import pendulum
 
 with DAG(
     'my-first-airflow-docker',
     description='A tutorial for your first Airflow DAG running on Docker',
     start_date=pendulum.today('UTC').add(days=-1),
-    schedule_interval='@daily'
+    # schedule_interval='@daily',
+    schedule_interval='*/5 * * * *',  # CRON
+    catchup=False
     ) as dag:
 
-    def hello_world():
-        print("Hello, World! With Airflow on Docker!")
+    def hello_world(data_interval_end):
+        print(f"Hello, World! With Airflow on Docker on day {data_interval_end} till 7 days more on {ds_add(data_interval_end, 7)}!")
 
     tarefa_1 = BashOperator(
         task_id = 'create_dir',
@@ -19,15 +22,16 @@ with DAG(
     )
     tarefa_2 = BashOperator(
         task_id = 'create_file',
-        bash_command = 'touch "/opt/airflow/test/test_dir/test.txt={{ data_interval_end }}"'
+        bash_command = 'touch "/opt/airflow/test/test_dir/test.txt={{ data_interval_end.strftime("%Y-%m-%d") }}"'
     )
     tarefa_3 = BashOperator(
         task_id = 'write_file',
         bash_command = 'echo "Hello, World!" > "/opt/airflow/test/test_dir/test.txt"'
     )
     tarefa_4 = PythonOperator(
-        task_id='hello_world',
-        python_callable=hello_world
+        task_id = 'hello_world',
+        python_callable = hello_world,
+        op_kwargs = {'data_interval_end': '{{ data_interval_end.strftime("%Y-%m-%d") }}'}
     )
         
     tarefa_1 >> tarefa_2 >> tarefa_3 >> tarefa_4
